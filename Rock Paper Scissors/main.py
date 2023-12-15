3edad57cad62daadd140b8dc3098509172bc1f91
G.Niruthian <88297426+GNiruthian@users.noreply.github.com>
2023-12-15 15:10:52 +0530

    Update main.py

--- Rock Paper Scissors/main.py
+++ Rock Paper Scissors/main.py
@@ -1,8 +1,8 @@
 def get_choices():
-    player_choice = "rock"
+    player_choice = input("Enter a choice (rock, paper, scissors)")
     computer_chocie = "paper"
-
-    return computer_chocie
+    choices = {"player": player_choice, "computer": computer_chocie}
+    return choices
 
 choices = get_choices()
 print(choices)
