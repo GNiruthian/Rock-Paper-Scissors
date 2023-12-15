077fded6437bb1c5fc9fa7f08724d0fd05495274
G.Niruthian <88297426+GNiruthian@users.noreply.github.com>
2023-12-15 15:23:58 +0530

    Update main.py

--- Rock Paper Scissors/main.py
+++ Rock Paper Scissors/main.py
@@ -1,10 +1,11 @@
 import random
 
 def get_choices():
-    player_choice = input("Enter a choice (rock, paper, scissors)")
-    computer_chocie = "paper"
+    player_choice = input("Enter a choice (rock, paper, scissors: )")
+    options = ["rock", "paper", "scissor"]
+    computer_chocie = random.choice(options)
     choices = {"player": player_choice, "computer": computer_chocie}
     return choices
 
-choices = get_choices()
-print(choices)
+def check_win(player, computer):
+    return [player, computer]
